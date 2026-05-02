bd95ddbda8dc7df4a1f8ad7ea999edcffc3f8156
poco_cpp <pocopococpp198@gmail.com>
2026-05-02 11:19:15 +0900

    update

--- scripts/release-all.ps1
+++ scripts/release-all.ps1
@@ -10,6 +10,10 @@ function Build-Version {
     dotnet build -c Release /p:BIZHAWK_HOME="$resolvedBizHawkPath/"
 
     $externalToolsPath = Join-Path $resolvedBizHawkPath "ExternalTools"
+
+    # __pycache__ を削除
+    Get-ChildItem -Path $externalToolsPath -Recurse -Directory -Filter "__pycache__" | Remove-Item -Recurse -Force
+
     $folderName = Split-Path $resolvedBizHawkPath -Leaf
     $zipPath = Join-Path $resolvedBizHawkPath "BizHawkPy-$folderName.zip"
     Compress-Archive -Path $externalToolsPath -DestinationPath $zipPath -Force
